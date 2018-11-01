3ffefe175bbb256f9dbe9d4d5e41211dc2d70b15
Abavish <vnand@csu.fullerton.edu>
2018-11-01 11:22:58 -0700

    added lodash module

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes')
 // console.log(os.userInfo().username);
+const _ = require('lodash')
 
-fs.appendFile('greetings.txt', `Hello, ${os.userInfo().username}! You are ${notes.age} years old.`, function (err){
-  if(err){
-    console.log('Unable to write to file');
-  }
-});
+// fs.appendFile('greetings.txt', `Hello, ${os.userInfo().username}! You are ${notes.age} years old.`, function (err){
+//   if(err){
+//     console.log('Unable to write to file');
+//   }
+// });
+
+console.log(_.uniq(['Vishal', 2,5,3,2,'Vishal']))
