65bdbe49ae4114777818082e406dd48ace67dc60
Abavish <vnand@csu.fullerton.edu>
2018-11-01 08:58:11 -0700

    playing around with modules and export

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes')
+// console.log(os.userInfo().username);
 
-fs.appendFile('greetings.txt', 'First Note');
+fs.appendFile('greetings.txt', `Hello, ${os.userInfo().username}! You are ${notes.age} years old.`, function (err){
+  if(err){
+    console.log('Unable to write to file');
+  }
+});
