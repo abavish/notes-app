806a2b9a2394821beed36122f5a42554afbf781c
Abavish <vnand@csu.fullerton.edu>
2018-11-03 12:27:44 -0700

    finished addNote function with duplicate check and error handling

--- notes.js
+++ notes.js
@@ -1,7 +1,27 @@
 console.log("Starting notes.js");
+const fs = require('fs')
 
 var addNote = (title, body) => {
-  console.log('Adding note', title, body)
+  var notes = []
+  var note = {
+    title,
+    body
+  }
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json')
+    notes = JSON.parse(notesString)
+  } catch(e){
+
+  }
+//store array of notes existing in array which have title as the one being inserted
+  var duplicateNotes = notes.filter((note) => note.title === title)
+
+  if(duplicateNotes.length === 0){
+    notes.push(note)
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+  }
+
 }
 
 var getAll = () => {
