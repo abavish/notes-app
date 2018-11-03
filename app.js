51781ad415e08bf2226ddaf784c696814846a006
Abavish <vnand@csu.fullerton.edu>
2018-11-03 01:02:06 -0700

    initial if else blocks

--- app.js
+++ app.js
@@ -4,5 +4,21 @@ const _ = require('lodash')
 
 const notes = require('./notes')
 
+var command = process.argv[2]
+console.log(`command executed: ${command}`)
 
-console.log(_.uniq(['Vishal']))
+if(command == 'add'){
+  console.log('Adding new node.');
+}
+else if(command == 'list'){
+  console.log(`Listing to all nodes.`);
+}
+else if(command == 'remove'){
+  console.log(`Deleting a node`);
+}
+else if(command == 'read'){
+  console.log(`Fetching a node.`);
+}
+else{
+  console.log('Command not recognized');
+}
